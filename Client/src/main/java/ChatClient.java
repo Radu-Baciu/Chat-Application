d49dea9ec7a8c497dc7873ee999ad1319d28bfa0
Radu-Baciu <81168417+Radu-Baciu@users.noreply.github.com>
2021-06-01 17:46:50 +0300

    Created ChatClient.java

---
+++ Client/src/main/java/ChatClient.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ChatClient extends Application {
+    public void init(Stage primaryStage) {
+        //TODO
+    }
+    public void start(Stage primaryStage) {
+        //TODO
+    }
+}
